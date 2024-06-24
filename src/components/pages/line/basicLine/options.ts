745c8bedbc0a866030189d25662a2f96f44a18a8
Todd_사봉석 <bssa@maxst.com>
2024-06-24 10:24:51 +0900

    [Line] * 배경색 설정을 통한 다크모드 * 차트 타이틀 설정

--- src/components/pages/line/basicLine/options.ts
+++ src/components/pages/line/basicLine/options.ts
@@ -3,6 +3,14 @@ import { LineSeriesOption } from 'echarts';
 type EChartsOption = echarts.ComposeOption<LineSeriesOption>;
 
 const LineOptions: EChartsOption = {
+  backgroundColor: '#27293d', // darkMode의 경우 배경 색상 지정
+  title: {
+    left: '50',
+    text: 'Basic Line',
+    target: 'blank',
+    link: 'https://www.naver.com',
+    textStyle: { fontStyle: 'oblique', color: 'white' },
+  },
   xAxis: {
     type: 'category',
     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
